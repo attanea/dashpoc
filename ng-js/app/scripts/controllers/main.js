aedbaeebfbb4010abe1f3bd8bba311a20fbd8277
f <f@montmasson.com>
2014-03-05 12:12:04 +0100

    Angularize list of applications: name and image

--- ng-js/app/scripts/controllers/main.js
+++ ng-js/app/scripts/controllers/main.js
@@ -2,9 +2,8 @@
 
 angular.module('dashboardApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+    $scope.applications = [
+        {'name':'Account Closing','image':'images/applications/icones_account_closing_64_44.png'},
+        {'name':'Secure Mail','image':'images/applications/icones_secure_mail_64_44.png'}
     ];
   });
