f20a53685428c410750c05cdd18bc5cd803eaf10
igor <igor.raizin@gmail.com>
2014-03-14 17:38:23 +0200

    added application template

--- react/src/main/webapp/resources/jsx/dashboard.js
+++ react/src/main/webapp/resources/jsx/dashboard.js
@@ -1,14 +1,20 @@
 /** @jsx React.DOM */
 
-var Comment = React.createClass({
+var Application = React.createClass({
     render: function () {
         return (
-            <div className="comment">
-                <h2 className="commentAuthor">
-          {this.props.name}
-                </h2>
-        {this.props.children}
-            </div>
+            <a class="dashboard_app_link" href="app_view.html">
+                <div class="dashboard_app dashboard_box">
+                    <i data-bind="css: {'fa-star': favourite(), 'fa-star-o': !favourite()}" class="fa"></i>
+
+                    <div class="dashboard_app_icon">
+                        <img src="http://localhost/img/app/icones_account_closing_64_44.png" data-bind="attr: {alt: name, title: name}"/>
+                    </div>
+                    <div class="dashboard_app_label">{this.props.name}</div>
+                    <div class="dashboard_app_label" style="diplay:none">{this.props.children}</div>
+
+                </div>
+            </a>
             );
     }
 });
@@ -16,7 +22,7 @@ var Comment = React.createClass({
 var CommentList = React.createClass({
     render: function () {
         var nodes = this.props.data.map(function (app) {
-            return <Comment name={app.author}>{app.text}</Comment>;
+            return <Application name={app.name}>{app.description}</Application>;
         });
 
         return (
@@ -46,7 +52,8 @@ var CommentBox = React.createClass({
             url: this.props.url,
             dataType: 'json',
             success: function (data) {
-                this.setState({data: data});
+                console.log(data);
+                this.setState({data: data.applications});
             }.bind(this),
 
             error: function (xhr, status, err) {
@@ -69,6 +76,7 @@ var CommentBox = React.createClass({
 });
 
 React.renderComponent(
-    <CommentBox url="resources/jsx/data.json"/>,
+//    <CommentBox url="resources/jsx/data.json"/>,
+    <CommentBox url="/dashboard/config/petzi"/>,
     document.getElementById('content')
 );
